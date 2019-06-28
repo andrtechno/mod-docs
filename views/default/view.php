40776cdac41725423d95821e9049f15ebad1e232
Andrew <andrew.panix@gmail.com>
2019-06-28 08:58:51 +0300

    init

--- views/default/view.php
+++ views/default/view.php
@@ -3,19 +3,10 @@ use panix\mod\docs\models\Docs;
 use panix\engine\CMS;
 use panix\engine\Html;
 
-?>
-
-<?php
-//$this->widget('ext.fancybox.Fancybox', array(
-//   'target' => 'a.fancybox',
-//));
-?>
-<?php
-
 
 $subManual = Docs::findOne($this->context->model->id)->children()->all();
-if (isset($subManual)) {
 
+if (isset($subManual)) {
     foreach ($subManual as $row) {
         ?>
         <div style="margin-bottom:20px;list-style: circle">
@@ -29,30 +20,19 @@ if (isset($subManual)) {
 
 
 <?php if ($this->context->model->description) { ?>
-<div class="manual-view">
-    <h1><?= ($this->h1) ? $this->h1 : $this->context->model->name; ?></h1>
-    <div class="tester content test">
-        <?= $this->context->model->description; ?>
-    </div>
-
-    <div class="card-footer">
-        <div class="date">
+    <div class="manual-view">
+        <h1><?= ($this->h1) ? $this->h1 : $this->context->model->name; ?></h1>
+        <div class="tester content test">
+            <?= $this->context->model->description; ?>
+        </div>
+        <div class="card-footer">
             <i class="icon-calendar"></i>
             <?php echo CMS::date($this->context->model->created_at) ?>
         </div>
-
     </div>
-    <?php
-    } else {
-
-        echo 'Информация составляется.';
-    }
-    ?>
-
-
-</div>
-
-
+<?php } else { ?>
+    <div class="alert alert-info">Информация составляется.</div>
+<?php } ?>
 
 
 
